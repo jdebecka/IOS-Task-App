24c5e2c3c40b974d42d468c2958d35042f705002
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-03-13 21:20:38 +0100

    initial commit

---
+++ TaskList/TaskList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Julia Debecka on 13/03/2020.
+//  Copyright © 2020 Julia Debecka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
